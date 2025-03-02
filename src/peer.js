bb98e4dfdaa43e6c81f10260e766e2ed5138726a
Mohit Kumar <mohit.janjadiya@gmail.com>
2025-03-02 13:06:43 +0530

    peer

--- src/peer.js
+++ src/peer.js
@@ -4,19 +4,8 @@ import Peer from "peerjs";
 
 export const createPeer = () => {
   return new Peer(undefined, {
-    host: "your-server.com",
-  port: 443,
-  path: "/peerjs",
-  secure: true,
-  config: {
-    iceServers: [
-      { urls: "stun:stun.l.google.com:19302" }, // Free STUN server
-      {
-        urls: "turn:relay.metered.ca:80",
-        username: "public",
-        credential: "public"
-      } // Google's free STUN server
-    ]
-  }
+     host: "0.peerjs.com",
+    port: 443,
+    secure: true
   });
 };
