8762b1f4d8db8c7cc0d94984d92ef892358dc600
Mohit Kumar <mohit.janjadiya@gmail.com>
2025-03-02 22:41:21 +0530

    peerf1

--- src/peer.js
+++ src/peer.js
@@ -4,7 +4,7 @@ import Peer from "peerjs";
 
 export const createPeer = () => {
   return new Peer(undefined, {
-     host: "0.peerjs.com",
+    host: "0.peerjs.com",
     port: 443,
     secure: true
   });
