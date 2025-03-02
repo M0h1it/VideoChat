2a1f441065d20edd1f0eb0dbcb45322f2d585c06
Mohit Kumar <mohit.janjadiya@gmail.com>
2025-03-02 13:04:14 +0530

    peer

--- src/peer.js
+++ src/peer.js
@@ -10,7 +10,12 @@ export const createPeer = () => {
   secure: true,
   config: {
     iceServers: [
-      { urls: "stun:stun.l.google.com:19302" }, // Google's free STUN server
+      { urls: "stun:stun.l.google.com:19302" }, // Free STUN server
+      {
+        urls: "turn:relay.metered.ca:80",
+        username: "public",
+        credential: "public"
+      } // Google's free STUN server
     ]
   }
   });
